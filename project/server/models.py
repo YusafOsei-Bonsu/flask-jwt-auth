0dd6d50a17e598d04df655e378fb08f04f0df74f
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-30 16:47:53 +0000

    A model for storing JWT tokens

--- project/server/models.py
+++ project/server/models.py
@@ -52,3 +52,18 @@ class User(db.Model):
         except jwt.InvalidTokenError:
             # The token is incorrect/malformed
             return "Invalid token. Please log in again."
+
+# Token model for storing JWT tokens
+class BlacklistToken(db.Model):
+    __tablename__ = 'blacklist_tokens'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+    blacklisted_on = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, token):
+        self.token = token
+        self.blacklisted_on = datetime.datetime.now()
+
+    def __repr__(self):
+        return '<id: token: {}'.format(self.token)
