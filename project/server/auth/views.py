61516848bc165549f43f8b2b7db3bf6a2eb1ff6c
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-30 15:41:19 +0000

    Writing a test to see what happens if the user already exists

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -6,7 +6,7 @@ from project.server.models import User
 
 auth_blueprint = Blueprint('auth', __name__)
 
-# User registration resource
+# A new user is registered and a new auth token for further requests is generated, which we send back to the client.
 class RegisterAPI(MethodView):
 
     def post(self):
@@ -48,5 +48,22 @@ registration_view = RegisterAPI.as_view('register_api')
 auth_blueprint.add_url_rule(
     '/auth/register',
     view_func=registration_view,
-    methods=['POST']
-)
+    methods=['POST'])
+
+# Test registration in a scenario where the user already exists
+def test_registered_with_already_registered_user(self):
+    user = User(email = 'joe@gmail.com', password = 'test')
+    db.session.add(user)
+    db.session.commit()
+
+    with self.client:
+        response = self.client.post(
+            '/auth/register',
+            data = json.dumps(dict(email = 'joe@gmail.com', password = '123456')),
+            content_type = 'application/json'
+        )
+        data = json.loads(response.data.decode())
+        self.assertTrue(data['status'] == 'fail')
+        self.assertTrue(data['message'] == 'User already exists. Please Log in.')
+        self.assertTrue(response.content_type == 'application/json')
+        self.assertEqual(response.status_code, 202)
