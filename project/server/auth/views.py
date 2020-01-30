14baccde670da8a18477e0ef70d9555fd35b9598
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-30 17:12:09 +0000

    Import BlackListToken

--- project/server/auth/views.py
+++ project/server/auth/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, make_response, jsonify
 from flask.views import MethodView
 
 from project.server import bcrypt, db
-from project.server.models import User
+from project.server.models import User, BlacklistToken
 
 auth_blueprint = Blueprint('auth', __name__)
 
