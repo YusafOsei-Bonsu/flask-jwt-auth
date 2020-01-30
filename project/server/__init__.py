1e21bf1d91bce548545b52073e6ecdea558a280c
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-30 15:19:16 +0000

    Registers the auth bp

--- project/server/__init__.py
+++ project/server/__init__.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from project.server.auth.views import auth_blueprint
 
 app = Flask(__name__)
 
@@ -16,3 +17,6 @@ app.config.from_object(app_settings)
 
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
+
+# Registering the authentication blueprint with the app
+app.register_blueprint(auth_blueprint)
