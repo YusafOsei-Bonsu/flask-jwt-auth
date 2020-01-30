63910e39eaf509d14b1aa98f49e5b2eae351854e
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2020-01-30 15:19:44 +0000

    Create test_auth.py

---
+++ project/tests/test_auth.py
@@ -0,0 +1,12 @@
+import unittest
+
+from project.server import db
+from project.server.models import User
+from project.tests.base import BaseTestCase
+
+# The test suite of the authentication process
+class TestAuthBlueprint(BaseTestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
